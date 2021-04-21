598fc83f11e2904840574c41d3d890532f25481d
Israel Sampaio <israelsampaio28@gmail.com>
2021-04-21 10:30:26 -0400

    feat: cria estado disabled para componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { View } from 'react-native'
 
 import Button from './styles'
 
 interface ButtonProps {
-    icon?: string
-    title?: string
-    onPress(): void
+  icon?: string
+  title?: string
+  onPress(): void
+  disabled?: boolean
 }
 
-const ButtonComponent: React.FC<ButtonProps> = ({icon, title, onPress}) => {
-    const hasIcon = icon != ""
+const ButtonComponent: React.FC<ButtonProps> = ({
+  icon,
+  title,
+  onPress,
+  disabled
+}) => {
+  return (
+    <View pointerEvents={disabled ? 'none' : 'auto'}>
+      <Button
+        onPress={onPress}
+        activeOpacity={0.7}
+        onlyIcon={!title && Boolean(icon)}
+        disabled={disabled}
+      >
+        {Boolean(icon) && <Button.Icon name={icon} size={24} />}
 
-    return (
-        <Button onPress={onPress} activeOpacity={0.7}>
-            {hasIcon && (
-                <Button.Icon name={icon} size={24} />
-            )}
-
-            {title && <Button.Text hasIcon={hasIcon}>{title}</Button.Text>}
-        </Button>
-    )
+        {title && <Button.Text hasIcon={Boolean(icon)}>{title}</Button.Text>}
+      </Button>
+    </View>
+  )
 }
 
 export default ButtonComponent
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,37 +1,50 @@
 import styled from 'styled-components/native'
-import {DefaultTheme, StyledComponentBase} from 'styled-components'
+import { DefaultTheme, StyledComponentBase } from 'styled-components'
 
-import {AntDesign} from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 
-interface Button extends StyledComponentBase<any, DefaultTheme>{
-    Icon?: any;
-    Text?: any;
+interface Button extends StyledComponentBase<any, DefaultTheme> {
+  Icon?: any
+  Text?: any
+}
+
+interface ButtonProps {
+  onlyIcon: boolean
+  disabled: boolean
 }
 
 interface ButtonTextProps {
-    hasIcon: boolean;
+  hasIcon: boolean
 }
 
-const ButtonStyled: Button = styled.TouchableOpacity`
-    background: ${props => props.theme.colors.green};
-    padding: 16px;
-    border-radius: 16px;
-    height: 56px;
-    width: 56px;
+const ButtonStyled: Button = styled.TouchableOpacity<ButtonProps>`
+  background: ${props => props.theme.colors.green};
+  padding: 16px;
+  border-radius: 16px;
+  height: 56px;
+
+  width: ${props => (props.onlyIcon ? '56px' : '100%')};
+
+  opacity: ${props => (props.disabled ? 0.5 : 1)};
+
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: row;
 `
 
 ButtonStyled.Icon = styled(AntDesign)`
-    color: ${props => props.theme.colors.white}
+  color: ${props => props.theme.colors.white};
 `
 
 ButtonStyled.Text = styled.Text<ButtonTextProps>`
-    font-family: 'Jost-Medium';
-    font-size: 17px;
-    text-align: center;
+  font-family: ${props => props.theme.fonts.complement};
+  font-size: 17px;
+  text-align: center;
 
-    margin-left: ${props => props.hasIcon ? '8px' : '0px'};
+  margin-left: ${props => (props.hasIcon ? '8px' : '0px')};
 
-    color: ${props => props.theme.colors.white}
-`;
+  color: ${props => props.theme.colors.white};
+`
 
 export default ButtonStyled
